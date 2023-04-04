eacfa216179b486bcda21680096c0364d0e09fe6
AdrielModollo <adrielmodollo@outlook.com>
2023-04-04 13:35:07 -0300

    feat: route authenticate

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import usersRouter from "../../modules/api/infra/http/routes/users.routes";
+import authenticateRouter from "../../modules/api/infra/http/routes/auth.routes";
 import { HttpException, HttpStatusCode } from "../exceptions/HttpException";
+import authenticate from "../../modules/api/infra/http/middlewares/authenticateMiddleware";
 
 const routes = Router();
 
+routes.use('/users', authenticateRouter)
+routes.use(authenticate);
 routes.use('/users', usersRouter);
 routes.use((req, res, next) => {
     next(new HttpException(HttpStatusCode.NOT_FOUND, 'Route not found'));
