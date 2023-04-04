218cb986d82d3a250c2d29622bca31062370971c
AdrielModollo <adrielmodollo@outlook.com>
2023-04-04 13:35:22 -0300

    feat: singleton authenticate

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,16 @@
 import { container } from 'tsyringe'
 import UsersRepository from '../../modules/api/infra/typeorm/repositories/UsersRepository'
 import IUsersRepository from '../../modules/api/repositories/IUsersRepository'
+import IAuthenticateRepository from '../../modules/api/repositories/IAuthenticateRepository'
+import AuthenticateRepository from '../../modules/api/infra/typeorm/repositories/AuthenticateRepository'
+
 
 container.registerSingleton<IUsersRepository>(
     'UsersRepository',
     UsersRepository
+)
+
+container.registerSingleton<IAuthenticateRepository>(
+    'AuthenticateRepository',
+    AuthenticateRepository
 )
