df710a6e992dfbd3981eede94b231d7935016130
AdrielModollo <adrielmodollo@outlook.com>
2023-04-05 20:01:09 -0300

    feat: add config unit tests

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: [
+        '<rootDir>/src',
+        '<rootDir>/__tests__'
+    ],
+    moduleDirectories: [
+        'node_modules',
+        'src'
+    ],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    testMatch: [
+        '**/__tests__/**/*.spec.(ts|tsx)'
+    ],
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/index.ts'
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: [
+        'text-summary',
+        'lcov'
+    ]
+};
