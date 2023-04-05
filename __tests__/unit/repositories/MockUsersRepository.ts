9920cecb42307aa1f7a369c8f9dc3706b987bc69
AdrielModollo <adrielmodollo@outlook.com>
2023-04-05 20:01:22 -0300

    feat: add mock repository

---
+++ __tests__/unit/repositories/MockUsersRepository.ts
@@ -0,0 +1,50 @@
+import User from "../../../src/modules/api/infra/typeorm/entities/User";
+import IUsersRepository from "../../../src/modules/api/repositories/IUsersRepository";
+import { ICreateUserDTO } from "../../../src/modules/api/dtos/ICreateUserDTO";
+import { IUpdateUserDTO } from "../../../src/modules/api/dtos/IUpdateUserDTO";
+
+class MockUsersRepository implements IUsersRepository {
+    private users: User[] = [];
+
+    async create({ name, email, password }: ICreateUserDTO): Promise<User> {
+        const user = new User();
+
+        Object.assign(user, {
+            id: '123',
+            name,
+            email,
+            password,
+        });
+
+        this.users.push(user);
+
+        return user;
+    }
+
+    async findByEmail(email: string): Promise<User | undefined> {
+        const user = this.users.find((user) => user.email === email);
+        return user;
+    }
+
+    async findById(id: string): Promise<User | undefined> {
+        const user = this.users.find((user) => user.id === id);
+        return user;
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        // TODO: Implement
+        throw new Error("Method not implemented.");
+    }
+
+    async updateUsers(id: string, data: Partial<IUpdateUserDTO>): Promise<User> {
+        // TODO: Implement
+        throw new Error("Method not implemented.");
+    }
+
+    async softDeleteByEmail(email: string): Promise<User> {
+        // TODO: Implement
+        throw new Error("Method not implemented.");
+    }
+}
+
+export default MockUsersRepository;
